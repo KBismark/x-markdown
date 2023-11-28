7e0b40e981ed625ad2abc35463196055dc22e760
KBis <bismarkkwabenayamoah@gmail.com>
2023-11-28 19:45:51 +0000

    Update

--- README.md
+++ README.md
@@ -1,2 +1,15 @@
 # xyntax
-Lightweight JavaScript syntax highlighter 
+Lightweight JavaScript syntax highlighter with zero performance overhead.    
+
+Xyntax is a lightweight, developer-friendly JavaScript package that takes syntax highlighting to the next level. Xyntax dynamically parses your code during development, generating pre-highlighted strings. This means zero performance issues for your documentation sites. 
+
+
+
+# Key Features
+
+- **Dynamic Parsing:** Code is parsed during development for real-time, browser-ready highlighting.    
+
+- **Zero performance overhead:** Minimal impact on performance, ensuring a smooth development experience.    
+
+` **Theme Flexibility:** Choose from a range of themes or customize your own for a personalized code highlighting.    
+
--- lib/patterns.ts
+++ lib/patterns.ts
@@ -1,13 +1,9 @@
 
-//const stringRand = `${replacerIdentifiers.string}${rand}_`
-export const stringsPattern = /('((?<=\\)'|[^'\n])*')|("((?<=\\)"|[^"\n])*")|(`((?<=\\)`|[^`])*`)/gs;
-//const commentRand = `${replacerIdentifiers.comment}${rand}_`
-export const commentsPattern = /(\/\*(.*?)\*\/)|(\/\/(.*?)\n)/gs;
 
+export const stringsPattern = /('((?<=\\)'|[^'\n])*')|("((?<=\\)"|[^"\n])*")|(`((?<=\\)`|[^`])*`)/gs;
+export const commentsPattern = /(\/\*(.*?)\*\/)|(\/\/(.*?)\n)/gs;
 export const xmarkdownPattern = /\/\/<xyntax\s+path\s*=\s*('\S+'|"\S+")\s+[a-zA-Z-0-9\$_]+>\s*{(.*?)}\s*\/\/<\/xyntax>/gs;
 export const xjsxPattern = /\s*\/\/<x-jsx>(.*?)\/\/<\/x-jsx>\s*\n/gs;
 export const xjsxJSPattern = /((?<=\/\/<>\s*){(.*?)})(?=\s*\/\/<\/>)/gs;
 export const xtextPattern = /<x-text>(.*?)<\/x-text>/gs;
-
 export const writePattern = /\/\/[ ]+\\write[ ]+[^\n]+\n\s*\S/gs;
-//const JSVariables = /[a-zA-Z$_][0-9a-zA-Z$_]*/gs;
